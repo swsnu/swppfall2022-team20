937b42f684324fb632ab295585fb1b0cab4275d8
DoohyunHwang97 <ghkdengus97@gmail.com>
2022-11-06 21:28:55 +0900

    view,url update

--- backend/clothes/urls.py
+++ backend/clothes/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-
+from django.contrib import admin
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path("signup/", views.signup),
+    path("login/", views.login),
     path('main/', views.main),
     path('reviews/<int:clothesId>/', views.reviewsList),
     #path('reviews/<int:clothesId>/<str:userName>/', views.review),
--- backend/clothes/views.py
+++ backend/clothes/views.py
@@ -1,8 +1,41 @@
-from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
+from django.http import HttpResponseBadRequest, HttpResponseNotAllowed, JsonResponse
+from django.shortcuts import redirect, render
+from django.contrib import auth
 from django.views.decorators.csrf import csrf_exempt
 import json
 from json.decoder import JSONDecodeError
 from .models import *
+
+@csrf_exempt       
+def signup(request):
+#    if request.method == 'GET':
+#        return render(request, "../../frontend/src/pages/Register/Register.tsx")
+    if request.method == 'POST':
+        user = Myuser.objects.create_user(username=request.POST.get('username'),
+        password=request.POST.get('password'),
+        nickname=request.POST.get('nickname'),
+        email=request.POST.get('email'),
+        chest_size=request.POST.get('chest_size'),
+        waist_size=request.POST.get('waist_size'),
+        thigh_size=request.POST.get('thigh_size'),
+        calf_size=request.POST.get('calf_size'))
+        user.save()
+
+        auth.login(request,user)
+        return JsonResponse(status=200)
+
+def login(request):
+#    if request.method == 'GET':
+#        return render(request, "../../frontend/src/pages/Signup.tsx")
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = auth.authenticate(request, username=username, password=password)
+
+        if user is not None:
+            auth.login(request,user)
+            return JsonResponse()
+        #else->error
 
 @csrf_exempt
 #모든 상품리스트 반환
@@ -55,7 +88,7 @@ def scrappedList(request, userId):
 
 #유저아이디가 {userId}인 유저의 스크랩된 상품에 상품을 추가
 @csrf_exempt
-def scrapItem(request, clotheId, userId):
-    
+def scrapItem(request, clothesId, userId):
+    User.objects.get(id=userId).scrapped.add(Clothes.objects.get(id=clothesId))
     return JsonResponse(status=201)
 
