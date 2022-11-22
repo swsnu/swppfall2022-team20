202330d1f9773a22071507a38f64338878c6a3fb
DoohyunHwang97 <ghkdengus97@gmail.com>
2022-11-22 21:59:36 +0900

    updated main(returns size data)

--- backend/clothes/views.py
+++ backend/clothes/views.py
@@ -50,8 +50,18 @@ def login(request):
 
 #모든 상품리스트 반환
 def main(request):
-    clothesList = [clothes for clothes in Clothes.objects.all().values()]
-    return JsonResponse(clothesList, safe=False, status=200)
+    clothes_data = []
+    for clothes_general_data in Clothes.objects.all().values():
+        clothes_general_data = clothes_general_data
+        clothes_id = clothes_general_data["id"]
+        clothes_size_data = []
+        for clothes_each_size_data in Size.objects.filter(clothes_id=clothes_id).values():
+            clothes_each_size_data.pop("id")
+            clothes_each_size_data.pop("clothes_id")
+            clothes_size_data.append(clothes_each_size_data)
+        clothes_general_data["size"] = clothes_size_data
+        clothes_data.append(clothes_general_data)
+    return JsonResponse(clothes_data, safe=False, status=200)
 
 def userprofile(request):
 #    if request.method == "GET":
